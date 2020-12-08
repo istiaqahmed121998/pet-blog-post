679d84a0856232568a3cf575d8eb7b032f5a1652
jhony <zenithzonezj@gmail.com>
2020-12-09 04:34:29 +0600

    Auth and Login

--- Router/api/user.js
+++ Router/api/user.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const jwt = require('jsonwebtoken');
 const config = require('config')
-const {check,validationResult} = require('express-validator/check');
+const {check,validationResult} = require('express-validator');
 const bcrypt=require('bcrypt');
 const routerUser = express.Router();
 
